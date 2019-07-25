e1f482512fc98f9526dd9cf6e8242a8b18d9dcee
Tai <taing89@gmail.com>
2019-07-24 20:34:44 -0700

    Added friends.js with some /friends/

---
+++ friends.js
@@ -0,0 +1,38 @@
+var friends = [
+
+{
+    name: "Mr. X",
+    photo: "hyperlinkgoeshere:D",
+    scores: [
+        "3",
+        "2",
+        "4",
+        "1",
+        "2",
+        "5",
+        "1",
+        "3",
+        "5",
+        "4",
+    ]
+},
+
+{
+    name: "Mrs. Z",
+    photo: "hyperlinkgoeshere:D",
+    scores: [
+        "4",
+        "3",
+        "5",
+        "2",
+        "3",
+        "1",
+        "2",
+        "4",
+        "1",
+        "5",
+    ]
+}
+
+]
+module.exports = friends;
